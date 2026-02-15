9e092788a7f242178f4365ca424a6ef563bdacee
nathanksimpson <nathanksimpson@gmail.com>
2026-02-15 16:38:58 +0900

    Update base path for webbudgetapp repository

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Change this to match your GitHub repository name, or '/' for root
-  base: process.env.NODE_ENV === 'production' ? '/Budget-Web-Based-App/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/webbudgetapp/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
